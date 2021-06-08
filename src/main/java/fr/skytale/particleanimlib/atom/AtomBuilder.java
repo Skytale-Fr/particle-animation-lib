bcbf47941f47520c3ce9d22deb340ebdb45409f9
AsmaChloe <asmachloe@gmail.com>
2021-06-08 20:40:34 +0200

    atom verif of attributes

--- src/main/java/fr/skytale/particleanimlib/atom/AtomBuilder.java
+++ src/main/java/fr/skytale/particleanimlib/atom/AtomBuilder.java
@@ -4,17 +4,20 @@ import fr.skytale.particleanimlib.attributes.ParticleTemplate;
 import fr.skytale.particleanimlib.parent.AAnimation;
 import fr.skytale.particleanimlib.parent.AAnimationBuilder;
 
-public class AtomBuilder extends AAnimationBuilder {
+public class AtomBuilder extends AAnimationBuilder<Atom> {
     private Atom atom;
 
     public AtomBuilder(){
         atom = new Atom();
+        atom.setStepAngle(15);
     }
 
     /*********SETTERS des éléments spécifiques de l'atome ***********/
 
     public void setStepAngle(double a){
-        atom.setStepAngle(a);
+        if(a==0)
+            throw new IllegalArgumentException("Step angle should not be equal to zero.");
+        animation.setStepAngle(a);
     }
 
     public void setSecondParticle(ParticleTemplate particle){
@@ -38,7 +41,17 @@ public class AtomBuilder extends AAnimationBuilder {
     }
 
     @Override
-    public AAnimation getAnimation() {
+    public Atom getAnimation() {
+        if (animation.getSecondParticle() == null) {
+            throw new IllegalArgumentException("Second particle should not be null.");
+        }
+
+        if(animation.getSphere()==null)
+            throw  new IllegalArgumentException("Sphere animation of Atom animation has to be initialized.");
+
+        if(animation.getCircle1()==null ||animation.getCircle2()==null || animation.getCircle3()==null)
+            throw  new IllegalArgumentException("All three circle animations of Atom animation has to be initialized.");
+
         return atom;
     }
 }
