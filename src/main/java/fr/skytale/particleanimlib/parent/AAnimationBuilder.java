bed4d1d1104bb303aef6ff75fed4a034a20d5a64
AsmaChloe <asmachloe@gmail.com>
2021-06-08 15:49:26 +0200

    AnimationBuilder generecity and verification

--- src/main/java/fr/skytale/particleanimlib/parent/AAnimationBuilder.java
+++ src/main/java/fr/skytale/particleanimlib/parent/AAnimationBuilder.java
@@ -6,17 +6,13 @@ import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
-public abstract class AAnimationBuilder {
-    protected AAnimation animation;
+public abstract class AAnimationBuilder<T extends AAnimation> {
+    protected T animation;
 
     public AAnimationBuilder() {
     }
 
     /*********SETTERS des éléments généraux d'une animation ***********/
-
-    public void setAnimation(AAnimation animation) {
-        this.animation = animation;
-    }
 
     public void setAnimationType(AnimationType animationType) {
         animation.setAnimationType(animationType);
@@ -42,5 +38,20 @@ public abstract class AAnimationBuilder {
         animation.setPlugin(javaPlugin);
     }
 
-    public abstract AAnimation getAnimation();
+    public T getAnimation() {
+        boolean hasMovingEntity = animation.getMovingEntity() != null;
+        if (animation.getLocation() != null && hasMovingEntity) {
+            throw new IllegalArgumentException("Fixed location and movingEntity should not be both defined");
+        }
+        if (hasMovingEntity && animation.getRelativeLocation() == null) {
+            animation.setRelativeLocation(new Vector(0, 0, 0));
+        }
+        if (animation.getMainParticle() == null) {
+            throw new IllegalArgumentException("Main particle should not be null");
+        }
+        if (animation.getPlugin() == null) {
+            throw new IllegalArgumentException("The plugin should be defined");
+        }
+        return animation;
+    };
 }
