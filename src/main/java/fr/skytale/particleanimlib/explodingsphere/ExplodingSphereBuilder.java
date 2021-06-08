b721f84ac23f45dba17836ae0ec9804be4b55297
AsmaChloe <asmachloe@gmail.com>
2021-06-08 16:15:44 +0200

    Exploding sphere builder verif of attributes

--- src/main/java/fr/skytale/particleanimlib/explodingsphere/ExplodingSphereBuilder.java
+++ src/main/java/fr/skytale/particleanimlib/explodingsphere/ExplodingSphereBuilder.java
@@ -2,44 +2,57 @@ package fr.skytale.particleanimlib.explodingsphere;
 
 import fr.skytale.particleanimlib.parent.AAnimation;
 import fr.skytale.particleanimlib.parent.AAnimationBuilder;
+import org.bukkit.util.Vector;
 
-public class ExplodingSphereBuilder extends AAnimationBuilder {
-    private ExplodingSphere explodingSphere;
+public class ExplodingSphereBuilder extends AAnimationBuilder<ExplodingSphere> {
 
     public ExplodingSphereBuilder(){
-        explodingSphere = new ExplodingSphere();
+        super();
+        animation = new ExplodingSphere();
+        animation.setGrowthSpeed(0.5);
+        animation.setExplosionLimit(6);
+        animation.setNbCircles(10);
+        animation.setRadius(1.0);
+        animation.setStepAngle(30);
     }
 
     /*********SETTERS des éléments spécifiques a la sphere explosante ***********/
 
     public  void setGrowthSpeed(double growthSpeed){
         if(growthSpeed<=0){
-            growthSpeed=0.5;
+            throw new IllegalArgumentException("Growth speed must be strictly positive.");
         }
-        explodingSphere.setGrowthSpeed(growthSpeed);
+        animation.setGrowthSpeed(growthSpeed);
     }
 
     public void setExplosionLimit(double explosionLimit){
-        if(explosionLimit<=1)       //TODO oe nn peut etre pas, si < à 1 on peut rappetissir la sphere
-            explosionLimit=6;
-        explodingSphere.setExplosionLimit(explosionLimit);
+        if(explosionLimit<=0)
+            throw new IllegalArgumentException("Explosion limit must be strictly positive.");;
+        animation.setExplosionLimit(explosionLimit);
     }
 
     public void setNbCircles(int nbCircles){
-        explodingSphere.setNbCircles(nbCircles);
+        if(nbCircles<=0)
+            throw new IllegalArgumentException("Number of circles must be positive.");
+        animation.setNbCircles(nbCircles);
     }
 
     public void setRadius(double radius) {
-        explodingSphere.setRadius(radius);
+        if (animation.getRadius() <= 0) {
+            throw new IllegalArgumentException("Radius should be positive.");
+        }
+        animation.setRadius(radius);
     }
 
     public void setStepAngle(double a){
-        explodingSphere.setStepAngle(a);
+        if(a==0)
+            throw new IllegalArgumentException("Step angle should not be equal to zero.");
+        animation.setStepAngle(a);
     }
 
     @Override
-    public AAnimation getAnimation() {
-        return explodingSphere;
+    public ExplodingSphere getAnimation() {
+        return super.getAnimation();
     }
 
 }
