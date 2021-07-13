633cc362916da60924e074d844f751ef8d3eb77b
kev <kevheritier@gmail.com>
2021-07-13 23:32:00 +0100

    Merge fixes

--- src/main/java/fr/skytale/particleanimlib/animation/animation/parabola/preset/ParabolaPlayerAimPresetExecutor.java
+++ src/main/java/fr/skytale/particleanimlib/animation/animation/parabola/preset/ParabolaPlayerAimPresetExecutor.java
@@ -6,6 +6,7 @@ import fr.skytale.particleanimlib.animation.attribute.position.APosition;
 import fr.skytale.particleanimlib.animation.attribute.projectiledirection.AnimationDirection;
 import fr.skytale.particleanimlib.animation.attribute.var.Constant;
 import fr.skytale.particleanimlib.animation.parent.preset.AAnimationPresetExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
 import java.awt.*;
@@ -17,7 +18,7 @@ public class ParabolaPlayerAimPresetExecutor extends AAnimationPresetExecutor<Pa
     }
 
     @Override
-    protected void apply(ParabolaBuilder parabolaBuilder) {
+    protected void apply(ParabolaBuilder parabolaBuilder, JavaPlugin plugin) {
         if (parabolaBuilder.getPosition() != null && parabolaBuilder.getPosition().getType() == APosition.Type.ENTITY) {
             Vector entityDirection = parabolaBuilder.getPosition().getMovingEntity().getLocation().getDirection();
             parabolaBuilder.setDirection(AnimationDirection.fromMoveVector(entityDirection));
--- src/main/java/fr/skytale/particleanimlib/animation/animation/parabola/preset/ParabolaPresetExecutor.java
+++ src/main/java/fr/skytale/particleanimlib/animation/animation/parabola/preset/ParabolaPresetExecutor.java
@@ -5,6 +5,7 @@ import fr.skytale.particleanimlib.animation.animation.parabola.ParabolaBuilder;
 import fr.skytale.particleanimlib.animation.attribute.ParticleTemplate;
 import fr.skytale.particleanimlib.animation.attribute.var.Constant;
 import fr.skytale.particleanimlib.animation.parent.preset.AAnimationPresetExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
 import java.awt.*;
@@ -16,7 +17,7 @@ public class ParabolaPresetExecutor extends AAnimationPresetExecutor<ParabolaBui
     }
 
     @Override
-    protected void apply(ParabolaBuilder parabolaBuilder) {
+    protected void apply(ParabolaBuilder parabolaBuilder, JavaPlugin plugin) {
         parabolaBuilder.setMainParticle(new ParticleTemplate("REDSTONE", new Color(255, 0, 0), null));
         parabolaBuilder.setTicksDuration(100);
         parabolaBuilder.setShowFrequency(new Constant<>(3));
