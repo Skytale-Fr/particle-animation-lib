b304fec4503215f913de0c8bc7e2b4a912ffc695
AsmaChloe <asmachloe@gmail.com>
2022-05-10 20:26:38 +0200

    hands of clock anim refacto

--- src/main/java/fr/skytale/particleanimlib/animation/animation/line/preset/LineHandsOfClockPresetExecutor.java
+++ src/main/java/fr/skytale/particleanimlib/animation/animation/line/preset/LineHandsOfClockPresetExecutor.java
@@ -25,18 +25,35 @@ public class LineHandsOfClockPresetExecutor extends AAnimationPresetExecutor<Lin
 
     @Override
     protected void apply(LineBuilder lineBuilder1, JavaPlugin plugin) {
+        
+        //Hour hand
+        lineBuilder1.setDirection(new Constant<>(new Vector(1,0,0)));
+        lineBuilder1.setLength(new Constant<>(2.0));
+        lineBuilder1.setMainParticle(new ParticleTemplate("REDSTONE", new Color(184, 115, 51), null));
+        lineBuilder1.setTicksDuration(60*20);
+        lineBuilder1.setShowPeriod(new Constant<>(2));
+        lineBuilder1.setNbPoints(new Constant<>(20));
+
+        lineBuilder1.setRotation(
+                new Vector(0, 0, 1),
+                new CallbackVariable<>( iterationCount -> {
+                    if(iterationCount%40==0){
+                        return Math.PI/12;
+                    }
+                    else{
+                        return Math.PI*2;
+                    }
+                }
+                )
+        );
+        Line mainHand = lineBuilder1.getAnimation();
+        
+
         //Minute hand
-        LineBuilder lineBuilder2 = new LineBuilder();
-        lineBuilder2.setPoint1AtOrigin();
-        lineBuilder2.setDirection(new Constant<>(new Vector(0,1,0)));
-        lineBuilder2.setLength(new Constant<>(3.0d));
-        lineBuilder2.setMainParticle(new ParticleTemplate("REDSTONE", new Color(184, 115, 51), null));
-        lineBuilder2.setTicksDuration(60*20);
-        lineBuilder2.setShowPeriod(new Constant<>(2));
-        lineBuilder2.setNbPoints(new Constant<>(30));
-        lineBuilder2.setPosition(lineBuilder1.getPosition());
-        lineBuilder2.setJavaPlugin(plugin);
-        lineBuilder2.setRotation(
+        lineBuilder1.setDirection(new Constant<>(new Vector(0,1,0)));
+        lineBuilder1.setLength(new Constant<>(3.0d));
+        lineBuilder1.setNbPoints(new Constant<>(30));
+        lineBuilder1.setRotation(
                 new Vector(0, 0, 1),
                 new CallbackVariable<>( iterationCount -> {
                     if(iterationCount%10==0){
@@ -48,29 +65,16 @@ public class LineHandsOfClockPresetExecutor extends AAnimationPresetExecutor<Lin
                 }
                 )
         );
-        Line minute_hand = lineBuilder2.getAnimation();
-        minute_hand.show();
+        Line minuteHand = lineBuilder1.getAnimation();
+        
+        lineBuilder1.setLength(0.01);
+        lineBuilder1.setNbPoints(1);
+        lineBuilder1.setTicksDuration(1);
+        lineBuilder1.setShowPeriod(0);
+        lineBuilder1.setCallback(animationEnding -> {
+            mainHand.show();
+            minuteHand.show();
+        });
 
-        //Hour hand
-        lineBuilder1.setPoint1AtOrigin();
-        lineBuilder1.setDirection(new Constant<>(new Vector(1,0,0)));
-        lineBuilder1.setLength(new Constant<>(2.0));
-        lineBuilder1.setMainParticle(new ParticleTemplate("REDSTONE", new Color(184, 115, 51), null));
-        lineBuilder1.setTicksDuration(60*20);
-        lineBuilder1.setShowPeriod(new Constant<>(2));
-        lineBuilder1.setNbPoints(new Constant<>(20));
-
-        lineBuilder1.setRotation(
-                new Vector(0, 0, 1),
-                new CallbackVariable<>( iterationCount -> {
-                        if(iterationCount%40==0){
-                            return Math.PI/12;
-                        }
-                        else{
-                            return Math.PI*2;
-                        }
-                    }
-                )
-        );
     }
 }
