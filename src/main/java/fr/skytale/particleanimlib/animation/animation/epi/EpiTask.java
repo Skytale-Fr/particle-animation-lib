e20bb7b7a9629c5095f7bcb99a4c098948ff4c04
AsmaChloe <asmachloe@gmail.com>
2022-12-19 14:05:52 +0100

    epi task done

--- src/main/java/fr/skytale/particleanimlib/animation/animation/epi/EpiTask.java
+++ src/main/java/fr/skytale/particleanimlib/animation/animation/epi/EpiTask.java
@@ -1,21 +1,18 @@
 package fr.skytale.particleanimlib.animation.animation.epi;
 
-import fr.skytale.particleanimlib.animation.attribute.RotatableVector;
 import fr.skytale.particleanimlib.animation.attribute.var.parent.IVariable;
 import fr.skytale.particleanimlib.animation.parent.task.AAnimationTask;
-import org.bukkit.Location;
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EpiTask extends AAnimationTask<Epi> {
-    @CheckIfChanged ({class=})
     private Integer nbPoints;
     private Double epiModifierNumerator;
     private Integer epiModifierDenominator;
     private Double radius;
     private Double maxRadius;
-
 
     public EpiTask(Epi epi) {
         super(epi);
@@ -24,32 +21,24 @@ public class EpiTask extends AAnimationTask<Epi> {
 
     @Override
     protected boolean hasAnimationPointsChanged() {
-        IVariable.ChangeResult<Integer> nbPointsChangeResult = animation.getNbPoints().willChange(iterationCount, nbPoints);
+        IVariable.ChangeResult<Integer> nbPointsChangeResult = hasAttributeChanged(nbPoints, animation.getNbPoints());
         nbPoints = nbPointsChangeResult.getNewValue();
-        IVariable.ChangeResult<Integer> nbPointChangeResult = hasAttributeChanged(nbPoints, animation.getNbPoints());
+        IVariable.ChangeResult<Double> epiModifierNumeratorChangeResult = hasAttributeChanged(epiModifierNumerator, animation.getEpiModifierNumerator());
+        epiModifierNumerator = epiModifierNumeratorChangeResult.getNewValue();
+        IVariable.ChangeResult<Integer> epiModifierDenominatorChangeResult = hasAttributeChanged(epiModifierDenominator, animation.getEpiModifierDenominator());
+        epiModifierDenominator = epiModifierDenominatorChangeResult.getNewValue();
+        IVariable.ChangeResult<Double> radiusChangeResult = hasAttributeChanged(radius, animation.getRadius());
+        radius = radiusChangeResult.getNewValue();
+        IVariable.ChangeResult<Double> maxRadiusResult = hasAttributeChanged(maxRadius, animation.getMaxRadius());
+        maxRadius = maxRadiusResult.getNewValue();
 
-        return angleBetweenEachPointChangeResult.hasChanged() || radiusChangeResult.hasChanged() || nbPointsChangeResult.hasChanged();
+        return nbPointsChangeResult.hasChanged() || epiModifierNumeratorChangeResult.hasChanged() || epiModifierDenominatorChangeResult.hasChanged() || radiusChangeResult.hasChanged() || maxRadiusResult.hasChanged();
     }
 
     @Override
     protected List<Vector> computeAnimationPoints() {
-        return null;
-    }
-
-    @SuppressWarnings("DuplicatedCode")
-    @Override
-    public void show(Location iterationBaseLocation) {
-        if (hasDurationEnded()) {
-            stopAnimation();
-            return;
-        }
-
-        double radius = animation.getRadius().getCurrentValue(iterationCount);
-        Double maxRadius = animation.getMaxRadius() != null ? animation.getMaxRadius().getCurrentValue(iterationCount) : null;
-        int nbPoints = animation.getNbPoints().getCurrentValue(iterationCount);
-        double epiModifierNumerator = animation.getEpiModifierNumerator().getCurrentValue(iterationCount);
-        int epiModifierDenominator = animation.getEpiModifierDenominator().getCurrentValue(iterationCount);
-        double epiModifier = epiModifierNumerator / epiModifierDenominator;
+        List<Vector> animationPoints = new ArrayList<>();
+        double epiModifier = epiModifierNumerator / epiModifierDenominator; //TODO CODE DUPLIQUE AVEC ROSE ET NODE
         double maxTheta;
         //defining maxTheta according to https://mathworld.wolfram.com/EpiCurve.html
         // If epiModifier is a rational number, then the curve closes at a computable polar angle.
@@ -73,23 +62,17 @@ public class EpiTask extends AAnimationTask<Epi> {
 
             final double radiusCosTheta = radius * Math.cos(theta) * distanceFromCenterToPoint;
             final double radiusSinTheta = radius * Math.sin(theta) * distanceFromCenterToPoint;
-            double x = iterationBaseLocation.getX() + (currentU.getX() * radiusCosTheta) + (currentV.getX() * radiusSinTheta);
-            double y = iterationBaseLocation.getY() + (currentU.getY() * radiusCosTheta) + (currentV.getY() * radiusSinTheta);
-            double z = iterationBaseLocation.getZ() + (currentU.getZ() * radiusCosTheta) + (currentV.getZ() * radiusSinTheta);
+            double x = U.getX() * radiusCosTheta + V.getX() * radiusSinTheta;
+            double y = U.getY() * radiusCosTheta + V.getY() * radiusSinTheta;
+            double z = U.getZ() * radiusCosTheta + V.getZ() * radiusSinTheta;
 
-            Location particleLocation = new Location(iterationBaseLocation.getWorld(), x, y, z);
+            Vector particleLocation = new Vector(x, y, z);
 
-            if (maxRadius == null || maxRadius >= particleLocation.distance(iterationBaseLocation)) {
-                showPoint(animation.getPointDefinition(), particleLocation, iterationBaseLocation);
+            if (maxRadius == null || maxRadius >= particleLocation.length()) {
+                animationPoints.add(particleLocation);
             }
         }
 
-        if (animation.getRotationAxis() != null) {
-            Vector rotationAxis = animation.getRotationAxis().getCurrentValue(iterationCount);
-            double rotationAngleAlpha = animation.getRotationAngleAlpha().getCurrentValue(iterationCount);
-
-            currentU = new RotatableVector(currentU).rotateAroundAxis(rotationAxis, rotationAngleAlpha);
-            currentV = new RotatableVector(currentV).rotateAroundAxis(rotationAxis, rotationAngleAlpha);
-        }
+        return animationPoints;
     }
 }
