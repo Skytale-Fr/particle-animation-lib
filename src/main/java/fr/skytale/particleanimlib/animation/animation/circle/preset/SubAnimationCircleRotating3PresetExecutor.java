8ea16d85aab23fa60a5349355653bdb5955d17c3
bZx <benjamin.boulaud@skytale.fr>
2023-07-01 12:42:38 +0200

    circle preset

--- src/main/java/fr/skytale/particleanimlib/animation/animation/circle/preset/SubAnimationCircleRotating3PresetExecutor.java
+++ src/main/java/fr/skytale/particleanimlib/animation/animation/circle/preset/SubAnimationCircleRotating3PresetExecutor.java
@@ -18,18 +18,17 @@ public class SubAnimationCircleRotating3PresetExecutor extends AAnimationPresetE
 
     @Override
     protected void apply(CircleBuilder circleBuilder, JavaPlugin plugin) {
-        circleBuilder.setRotation(new Vector(1, 0, 0), new Vector(0, 1, 0));
         circleBuilder.setNbPoints(5, true);
         circleBuilder.setRadius(1);
         circleBuilder.setTicksDuration(20);
         circleBuilder.setShowPeriod(new Constant<>(2));
         circleBuilder.setRotation(new Vector(0, 0, 1), Math.PI / 25);
-        Circle circle = circleBuilder.getAnimation();
+        Circle subCircle = circleBuilder.getAnimation();
         circleBuilder.setNbPoints(8, true);
         circleBuilder.setRotation(new Vector(0, 0, 1), Math.PI / 25);
         circleBuilder.setRadius(8);
         circleBuilder.setTicksDuration(400);
         circleBuilder.setShowPeriod(new Constant<>(20));
-        circleBuilder.setPointDefinition(new SubAnimPointDefinition(circle, new SubAnimOrientationConfig(SubAnimOrientationModifier.NO_ADDITIONAL_ORIENTATION)));
+        circleBuilder.setPointDefinition(new SubAnimPointDefinition(subCircle, new SubAnimOrientationConfig(SubAnimOrientationModifier.NO_ADDITIONAL_ORIENTATION)));
     }
 }
